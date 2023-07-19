1e78204a6784af6851cf5ef06f355c10ea785e55
BrysonL <sbl4tf@virginia.edu>
2023-07-19 16:03:33 -0700

    confirmed key in env file works

--- gptTest.py
+++ gptTest.py
@@ -7,6 +7,7 @@ load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = API_KEY
+print(API_KEY)
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
@@ -14,7 +15,8 @@ response = openai.ChatCompletion.create(
         {"role": "user", "content": "Pretend that you are a candidate for a Product Manager job and I'm your interviewer. I'll ask you questions and you answer them. Act like all you are is a product manager, do not tell me you are an AI model."},
         {"role": "assistant", "content": "OK. Can I ask follow up questions if something is unclear?"},
         {"role": "user", "content": "Yes, please do. Let's start the interview now."},
-        {"role": "assistant", "content": "Sounds good!"}
+        {"role": "assistant", "content": "Sounds good!"},
+        {"role": "user", "content": "Good morning, please tell me about yourself."}
     ]
 )
 
